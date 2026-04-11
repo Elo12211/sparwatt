18da507fdc76a0b5928917c3008d1b55ce2158bd
Elo12211 <ole.andreas.sneltvedt@gmail.com>
2026-04-11 20:55:00 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -46,7 +46,7 @@ module.exports = async function handler(req, res) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        model: 'llama3-8b-8192',
+        model: 'llama-3.1-8b-instant',
         messages: [{ role: 'system', content: systemPrompt }].concat(messages),
         max_tokens: 400,
         temperature: 0.7
